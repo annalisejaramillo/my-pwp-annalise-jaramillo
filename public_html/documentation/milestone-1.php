dc122b38b53a034732cdb175b325d893418416b3
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-07-15 20:36:08 -0600

    started html in milestone 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<div>
+			<h1>PWP Milestone 1 Documentation</h1>
+		</div>
+		<br>
+		<div>
+			<h2>Purpose, Audience, Goal</h2>
+			<h3>Purpose</h3>
+			<p>This project is to serve as a business website for M is for Macaron.</p>
+			<br>
+			<h3>Audience</h3>
+			<p>This website is designed with customers in mind, and is targeted to individuals in the event business, </p>
+			<p>the target audience is to include, wedding planners, party planners, dessert lovers</p>
+			<br>
+			<h3>Goal</h3>
+			<p></p>
+		</div>
+	</body>
+</html>
